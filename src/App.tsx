6dc63310a617cfd81a41fccfe8b2a103c5e0e222
bhagesh <dbhagesh@gmail.com>
2023-05-12 01:37:32 +0530

    feat : Primer Toolbar added with theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,90 @@
-import { useState } from 'react'
-import { Slider } from './index';
-import reactLogo from './assets/react.svg'
-
-import viteLogo from '/vite.svg'
-
-import './App.css'
+import {
+  ThemeProvider,
+  BaseStyles,
+  Box,
+  ToggleSwitch,
+  Text,
+  IconButton,
+} from "@primer/react";
+import { SearchIcon, ThreeBarsIcon, GearIcon } from "@primer/octicons-react";
+import { Slider, Toolbar } from "./index";
+import "./App.css";
+import { Fragment, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false);
   return (
     <>
-      <Slider/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider colorMode={isOn ? "day" : "night"}>
+        <BaseStyles>
+          <Fragment>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              flexDirection="column"
+              sx={{ height: "100%", width: "100%" }}
+            >
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-around"
+              >
+                <Text as={"h2"}>Primer AddOns</Text>
+                <Box>
+                  <Text>Lights</Text>
+                  <ToggleSwitch
+                    onClick={() => setIsOn(!isOn)}
+                    checked={isOn}
+                    aria-labelledby="switchLabel"
+                  />
+                </Box>
+              </Box>
+              <Box>
+                <Slider />
+                <Toolbar
+                  heading={
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      sx={{ gap: "0.5rem" }}
+                    >
+                      <IconButton
+                        aria-label="Search"
+                        icon={ThreeBarsIcon}
+                        size="small"
+                        variant="invisible"
+                      />
+                      <Text>Datalayer Toolbar</Text>
+                    </Box>
+                  }
+                  align="flex-end"
+                  border={true}
+                  gap="1rem"
+                  padding="0 1rem"
+                >
+                  <IconButton
+                    aria-label="Search"
+                    icon={SearchIcon}
+                    size="small"
+                  />
+                  <Text>Action1</Text>
+                  <Text>Action2</Text>
+                  <Text>Action3</Text>
+                  <IconButton
+                    aria-label="Search"
+                    icon={GearIcon}
+                    size="small"
+                    variant="invisible"
+                  />
+                </Toolbar>
+              </Box>
+            </Box>
+          </Fragment>
+        </BaseStyles>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/toolbar/toolbar.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Box } from "@primer/react";
+interface Props {
+  align?:
+    | "center"
+    | "flex-start"
+    | "flex-end"
+    | "space-evenly"
+    | "space-around"
+    | "space-between";
+  gap?: string;
+  height?: string;
+  padding?: string;
+  heading?: React.ReactNode; // Preferred a Text Component of Primer
+  backgroundColor?: string;
+  borderBottomHeight?: number;
+  borderBottomColor?: string;
+  border?: boolean;
+  children: React.ReactNode | React.ReactNode[];
+}
+const Toolbar = ({
+  align = "flex-start",
+  height = "5vh",
+  gap = "0.1rem",
+  padding,
+  heading,
+  backgroundColor,
+  borderBottomHeight = 1,
+  borderBottomColor,
+  border = false,
+  children,
+}: Props) => {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      sx={{
+        backgroundColor: backgroundColor ? backgroundColor : "pageHeaderBg",
+        height: height,
+        width: "100%",
+        padding: padding,
+        borderBottomWidth: borderBottomHeight,
+        borderBottomStyle: border ? "solid" : "none",
+        borderColor: borderBottomColor ? borderBottomColor : "border.default",
+      }}
+    >
+      <Box display="flex">
+        {heading != null && heading != undefined ? heading : <></>}
+      </Box>
+      <Box
+        display="flex"
+        flexGrow={1}
+        alignItems="center"
+        justifyContent={align}
+        sx={{
+          gap: gap,
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default Toolbar;

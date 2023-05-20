bcb464c59ff4ea151c004c009ea1cc366dab52e2
Eric Charles <eric@datalayer.io>
2023-05-20 19:34:19 +0200

    chore: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import {
   BaseStyles,
   Box,
   Text,
-  ThemeProvider,
+  ThemeProvider
 } from "@primer/react";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider colorMode="night">
       <BaseStyles>
-        <Box>
-          <Text>Primer Addons</Text>
+        <Box bg="canvas.default">
+          <Box>
+            <Text>Primer React</Text>
+          </Box>
+            <Box>
+              <Text>Addons</Text>            
+            </Box>
         </Box>
       </BaseStyles>
     </ThemeProvider>
-  );
+  )
 }
 
 export default App;

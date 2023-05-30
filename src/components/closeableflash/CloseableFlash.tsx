001cc37875a5fb9f74d0cfc31cbd1fc1d732dc02
im-adithya <imadithyavardhan@gmail.com>
2023-05-30 12:53:29 +0530

    feat: improve flash component

--- src/components/closeableflash/CloseableFlash.tsx
+++ src/components/closeableflash/CloseableFlash.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { Box, Flash, FlashProps } from "@primer/react";
-import { XIcon } from "@primer/octicons-react";
+import { Box, Flash, FlashProps, StyledOcticon, Text } from "@primer/react";
+import { Icon, XIcon } from "@primer/octicons-react";
 
 type CloseableFlashProps = FlashProps & {
   onClose: () => void;
+  leadingIcon: Icon | null;
 }
 
 export const CloseableFlash: React.FC<CloseableFlashProps> = (props) => {
+  const {leadingIcon, variant, ...otherProps} = props;
   const [isVisible, setIsVisible] = useState(true);
 
   const handleClose = () => {
@@ -17,11 +19,13 @@ export const CloseableFlash: React.FC<CloseableFlashProps> = (props) => {
   if (!isVisible) {
     return null;
   }
-
   return (
-    <Flash {...props}>
+    <Flash variant={variant} {...otherProps}>
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Box>{props.children}</Box>
+        <Box sx={{ flexGrow: 1 }}>
+          {leadingIcon && <StyledOcticon icon={leadingIcon} />}
+          <Text color={`${variant === "default" ? "accent" : (variant === "warning" ? "attention" : variant)}.fg`}>{props.children}</Text>
+        </Box>
         <Box onClick={handleClose} sx={{cursor: "pointer"}}>
           <XIcon size={20}/>
         </Box>

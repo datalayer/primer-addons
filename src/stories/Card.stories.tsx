d703abf14f93abfa00f34fdde159eed3564b9dcc
im-adithya <imadithyavardhan@gmail.com>
2023-05-31 12:34:44 +0530

    chore: replace image

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -57,7 +57,7 @@ export const CardDay: Story = {
       description="September 14, 2016"
       action={<IconButton aria-label="Menu" onClick={() => alert("Menu")} icon={ThreeBarsIcon} />}
     />
-    <Card.Image height={200} image="https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=saahil-khatkhate-kfDsMDyX1K0-unsplash.jpg&w=640"/>
+    <Card.Image height={200} image="https://images.unsplash.com/photo-1623961990059-28356e226a77?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=douglas-lopez-4B0cLMtJxWw-unsplash.jpg&w=640"/>
     <Card.Content>
       <Text display="block" fontSize={22}>Paella</Text>
       <Text color="fg.muted">
@@ -87,7 +87,7 @@ export const CardNight: Story = {
     description="September 14, 2016"
     action={<IconButton aria-label="Menu" onClick={() => alert("Menu")} icon={ThreeBarsIcon} />}
   />
-  <Card.Image height={200} image="https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=saahil-khatkhate-kfDsMDyX1K0-unsplash.jpg&w=640"/>
+  <Card.Image height={200} image="https://images.unsplash.com/photo-1623961990059-28356e226a77?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=douglas-lopez-4B0cLMtJxWw-unsplash.jpg&w=640"/>
   <Card.Content>
     <Text display="block" fontSize={22}>Paella</Text>
     <Text color="fg.muted">
